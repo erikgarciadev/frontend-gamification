907cee3f8a81c9830e554068a4ba3063c919ca0d
Erik Garcia Tacas <erik.fisi16@gmail.com>
2022-08-25 23:02:03 -0500

    feat: add chakra

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
+import { Box, Text } from "@chakra-ui/react";
+import React from "react";
 
 function App() {
   return (
-    <div >
-      <h1>Hello world</h1>
-    </div>
+    <Box>
+      <Text fontWeight={"bold"} fontSize={"4xl"}>
+        Hello world
+      </Text>
+    </Box>
   );
 }
 
