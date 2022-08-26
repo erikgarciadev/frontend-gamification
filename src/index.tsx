329830ce3affdad97154ba381f5f710554e793e6
Erik Garcia Tacas <erik.fisi16@gmail.com>
2022-08-25 23:07:43 -0500

    feat: add redux

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,20 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { ChakraProvider } from '@chakra-ui/react';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { ChakraProvider } from "@chakra-ui/react";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <ChakraProvider>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </ChakraProvider>
 );
 
